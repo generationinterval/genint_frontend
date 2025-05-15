1d272babc83cf7afffdce0d456ab79adeae60622
Davidmrn5 <davidmartinpest@yahoo.es>
2025-05-15 16:19:35 +0200

    clean router

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,13 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import { HomePage } from "@/pages/home";
-import { AboutPage } from "@/pages/about";
-import { SummStatInd } from "@/pages/sum_stats_ind";
-import { SummStatGroup } from "@/pages/sum_stats_group";
-import { SummStatFrag } from "@/pages/sum_stats_frag";
-import { RegSeqCum } from "@/pages/reg_seq_cum";
-import { HMMProb } from "@/pages/hmm_prob";
-import { OutgroupFilter } from "@/pages/outgroup_filter";
-import { paths } from "@/paths";
 import { Layout } from "@/layout/layout";
+import { AboutPage } from "@/pages/about";
 import { FragVisInd } from "@/pages/frag_vis_ind";
 import { FragVisReg } from "@/pages/frag_vis_reg";
+import { HomePage } from "@/pages/home";
+import { SummStatFrag } from "@/pages/sum_stats_frag";
+import { SummStatInd } from "@/pages/sum_stats_ind";
+import { paths } from "@/paths";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 export const AppRouter: React.FC<NonNullable<unknown>> = () => {
   return (
@@ -20,19 +16,9 @@ export const AppRouter: React.FC<NonNullable<unknown>> = () => {
         <Route path="/" element={<HomePage />} />
         <Route path={paths.others.about} element={<AboutPage />} />
         <Route path={paths.summary_stats.per_ind} element={<SummStatInd />} />
-        <Route
-          path={paths.summary_stats.per_group}
-          element={<SummStatGroup />}
-        />
         <Route path={paths.summary_stats.per_frag} element={<SummStatFrag />} />
         <Route path={paths.fragment.vis_per_ind} element={<FragVisInd />} />
         <Route path={paths.fragment.vis_per_reg} element={<FragVisReg />} />
-        <Route path={paths.others.reg_seq_cum} element={<RegSeqCum />} />
-        <Route path={paths.others.hmm_prob} element={<HMMProb />} />
-        <Route
-          path={paths.others.outgroup_filter}
-          element={<OutgroupFilter />}
-        />
       </Route>
     </Routes>
   );
